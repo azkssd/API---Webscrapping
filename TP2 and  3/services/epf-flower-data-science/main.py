9049407fd59da449cbe04b44e6c9d622ac101fb5
AnneZoe <anne-zoe.kassidonis@epfedu.fr>
2024-11-26 15:17:33 +0100

    redirected the root of the api to swagger docs

--- TP2 and  3/services/epf-flower-data-science/main.py
+++ TP2 and  3/services/epf-flower-data-science/main.py
@@ -2,7 +2,15 @@ import uvicorn
 
 from src.app import get_application
 
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
 app = get_application()
+
+# Redirect the root URL to the Swagger docs
+@app.get("/")
+def root():
+    return RedirectResponse(url="/docs")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", debug=True, reload=True, port=8080)
